37e38a1b41a50f3c5d977dab198c15458e0669cf
mahikashyap28 <155059328+mahikashyap28@users.noreply.github.com>
2023-12-28 14:27:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// function mahi(msg){
+//     console.log(msg)
+// }
+// mahi("hello bby")
+
+
+
+// function mahi(x,y){
+//     console.log(x+y)
+// }
+// mahi(4,8)
+
+function mahi(msg){
+    console.log(msg)
+}
+mahi("heyyy")
+
+
+
+
+
+
+
